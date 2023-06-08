329d55504e013734de1c0583b276eec746417c91
bpena707 <bpena.610@gmail.com>
2023-06-07 20:41:07 -0700

    sidebar and main page
    updated the sidbar to include a home and search button with active routes. also rendered the main content as a children that are then rendered onto the main page.

--- components/Box.tsx
+++ components/Box.tsx
@@ -1,5 +1,7 @@
 /* this is the box component that is passed into the sidebar and holds the children component that is wrapped */
 
+import { twMerge } from "tailwind-merge"
+
 interface BoxProps {
     children: React.ReactNode
     className?: string
@@ -10,7 +12,8 @@ const Box: React.FC<BoxProps> = ({
     className
 }) => {
   return (
-    <div>
+    // this div styles the sidbar as a whole. additional className props can also be passed
+    <div className={twMerge(`bg-neutral-900 rounded-lg h-fit w-full`, className)}>
         {children}
     </div>
   )
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,7 +7,10 @@ import { useMemo } from "react"
 import { HiHome } from "react-icons/hi"
 import { BiSearch } from "react-icons/bi"
 import Box from "./Box"
+import SidebarItem from "./SidebarItem"
+import Library from "./Library"
  
+//the interface props passes the children as React node which is a type that defines what nodes can be rendered
 interface SidebarProps{
     children: React.ReactNode
 }
@@ -17,13 +20,14 @@ const Sidebar: React.FC<SidebarProps> = ({
 }) => {
     const pathname = usePathname()
 
-    // declares an array of possible routes in the project
+    // declares an array of possible routes in the project based on the sidebar
     const routes = useMemo(() => [
         {
             // the home pathname will be active as long as we are not in the search
             icon: HiHome,
             label: 'Home',
             active: pathname !== '/search',
+            href: '/'
         },
         {
             icon: BiSearch,
@@ -36,9 +40,24 @@ const Sidebar: React.FC<SidebarProps> = ({
     <div className="flex h-full">
         <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
             <Box>
-                Sidebar Navigation
+                <div className="flex flex-col gap-y-4 px-5 py-4">
+                    {routes.map((item) => (
+                        <SidebarItem 
+                            key={item.label}
+                            {...item}
+                        />
+                    ))}
+                </div>
+            </Box>
+            {/* this box is for the song library that sits under the navigation and fills the side which is passed into sidebar component */}
+            <Box className="overflow-y-auto h-full">
+                <Library />
             </Box>
         </div>
+        {/* the main element renders the children onto the page */}
+        <main className="h-full flex-1 overflow-y-auto py-2">
+            {children}
+        </main>
     </div>
   )
 }
