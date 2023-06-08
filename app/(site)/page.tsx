77ef8947d56f86d3ccd33fa0d9469336d3d65ef0
bpena707 <bpena.610@gmail.com>
2023-06-07 22:31:12 -0700

    header
    working on the header component. included styling and adding in the navigation icon.

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,15 @@
+import Header from "@/components/Header";
+
+
+
 /* this page file was moved to this folder rather than having it straight in the app directory */
 export default function Home() {
   
   return (
-    <div className="text-green-500 ">Main Content</div>
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <Header>
+        
+      </Header>
+    </div>
   )
 }
--- components/Library.tsx
+++ components/Library.tsx
@@ -1,8 +1,33 @@
 'use client'
 
+import { TbPlaylist } from 'react-icons/tb'
+import { AiOutlinePlus } from "react-icons/ai"
+ 
 const Library = () => {
+    const onClick = () => {
+        // TODO: handle click functionality
+    }
   return (
-    <div>Library</div>
+    // this div is for the overall sidebar component and applies flex 
+    <div className='flex flex-col'>
+        {/* this div holds the playlist and the plus button */}
+        <div className='flex items-center justify-between px-5 pt-4'>
+            {/* this div is for the playlist icon and the library */}
+            <div className='inline-flex items-center gap-x-2'>
+                <TbPlaylist size={26} className='text-neutral-400' />
+                <p className='' >Your Library</p>
+            </div>
+            <AiOutlinePlus 
+                onClick={onClick}
+                size={20} 
+                className='text-neutral-400 cursor-pointer hover:text-white transition'
+            />
+        </div>
+        {/* this div is for the list of songs section */}
+        <div className='flex flex-col gap-y-2 mt-4 px-3'>
+            List of Songs 
+        </div>
+    </div>
   )
 }
 
